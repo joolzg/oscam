joolzg/oscam

commit 6cd19a08fe97efa3f96d73260841ab6715b5d550

save reader answers in extra struct and not in ecmtask so it is not needed to copy the ecm request before sending it to the reader. use same rc codes for reader and ecm requests.
git-svn-id: http://streamboard.gmc.to/svn/oscam/trunk@5739 4b0bc96b-bc66-0410-9d44-ebda105a78c1

reader-videoguard2.c
@@ -44,7 +44,7 @@ static void dimeno_PostProcess_Decrypt(struct s_reader * reader, unsigned char *
   }
 }
 
-static void do_post_dw_hash(unsigned char *cw, unsigned char *ecm_header_data)
+static void do_post_dw_hash(unsigned char *cw, const unsigned char *ecm_header_data)
 {
   int32_t i, ecmi, ecm_header_count;
   unsigned char buffer[0x80];
@@ -569,7 +569,7 @@ static int32_t videoguard2_card_init(struct s_reader * reader, ATR newatr)
   return OK;
 }
 
-static int32_t videoguard2_do_ecm(struct s_reader * reader, ECM_REQUEST *er)
+static int32_t videoguard2_do_ecm(struct s_reader * reader, const ECM_REQUEST *er, struct s_ecm_answer *ea)
 {
   unsigned char cta_res[CTA_RES_LEN];
   unsigned char ins40[5] = { 0xD1,0x40,0x00,0x80,0xFF };
@@ -579,7 +579,7 @@ static int32_t videoguard2_do_ecm(struct s_reader * reader, ECM_REQUEST *er)
   unsigned char tbuff[264], rbuff[264];
   tbuff[0]=0;
 
-  memset(er->cw+0,0,16); //set cw to 0 so client will know it is invalid unless it is overwritten with a valid cw
+  memset(ea->cw+0,0,16); //set cw to 0 so client will know it is invalid unless it is overwritten with a valid cw
   memcpy(tbuff+1,er->ecm+posECMpart2+1,lenECMpart2-1);
 
 /*
@@ -635,7 +635,7 @@ static int32_t videoguard2_do_ecm(struct s_reader * reader, ECM_REQUEST *er)
       }
 
       // copy cw1 in place
-      memcpy(er->cw+0,rbuff+5,8);
+      memcpy(ea->cw+0,rbuff+5,8);
 
       // process cw2
       unsigned char *payload = rbuff+5;
@@ -647,7 +647,7 @@ static int32_t videoguard2_do_ecm(struct s_reader * reader, ECM_REQUEST *er)
         {
           case 0x25:  // CW2
             //cs_dump (payload + ind, payload[ind+1]+2, "INS54 - CW2");
-            memcpy(er->cw+8,&payload[ind+3],8);
+            memcpy(ea->cw+8,&payload[ind+3],8);
             ind += payload[ind+1]+2;
             break;
 
@@ -660,11 +660,11 @@ static int32_t videoguard2_do_ecm(struct s_reader * reader, ECM_REQUEST *er)
 
       if (new_len != lenECMpart2)
       {
-         memcpy(er->cw, er->cw+8, 8);
-         memset(er->cw+8, 0, 8);
+         memcpy(ea->cw, ea->cw+8, 8);
+         memset(ea->cw+8, 0, 8);
       }
       // fix for 09ac cards
-      dimeno_PostProcess_Decrypt(reader, rbuff, er->cw);
+      dimeno_PostProcess_Decrypt(reader, rbuff, ea->cw);
 
       //test for postprocessing marker
       int32_t posB0 = -1;
@@ -676,22 +676,22 @@ static int32_t videoguard2_do_ecm(struct s_reader * reader, ECM_REQUEST *er)
         }
       }
       if (posB0 != -1) {
-        do_post_dw_hash( er->cw+0, &er->ecm[posB0-2]);
-        do_post_dw_hash( er->cw+8, &er->ecm[posB0-2]);
+        do_post_dw_hash( ea->cw+0, &er->ecm[posB0-2]);
+        do_post_dw_hash( ea->cw+8, &er->ecm[posB0-2]);
       }
 
       if (reader->caid == 0x0907) { //quickfix: cw2 is not a vaild cw, something went wrong before
-        memset(er->cw+8, 0, 8);
+        memset(ea->cw+8, 0, 8);
         if (er->ecm[0] & 1) {
-          memcpy(er->cw+8, er->cw, 8);
-          memset(er->cw, 0, 8);
+          memcpy(ea->cw+8, ea->cw, 8);
+          memset(ea->cw, 0, 8);
         }
       } else {
         if(er->ecm[0]&1) {
           unsigned char tmpcw[8];
           memcpy(tmpcw,er->cw+8,8);
-          memcpy(er->cw+8,er->cw+0,8);
-          memcpy(er->cw+0,tmpcw,8);
+          memcpy(ea->cw+8,ea->cw+0,8);
+          memcpy(ea->cw+0,tmpcw,8);
         }
       }
 

global-functions.h
@@ -80,7 +80,7 @@ extern void cs_disconnect_client(struct s_client *);
 extern int32_t check_cwcache2(ECM_REQUEST *, uint64_t grp);
 extern int32_t write_to_pipe(struct s_client *, int32_t, uchar *, int32_t);
 extern int32_t read_from_pipe(struct s_client *, uchar **);
-extern int32_t write_ecm_answer(struct s_reader *, ECM_REQUEST *);
+extern int32_t write_ecm_answer(struct s_reader *, ECM_REQUEST *, int8_t, uint8_t, uchar *, char *);
 extern uint32_t chk_provid(uchar *, uint16_t);
 extern void convert_to_beta(struct s_client *cl, ECM_REQUEST *er, uint16_t caidto);
 #ifdef IRDETO_GUESSING
@@ -265,7 +265,7 @@ extern void cs_log_int(uint16_t mask, int8_t lock, const uchar *buf, int32_t n, 
 #define cs_ddump_mask(...) nop()
 #endif
 extern void log_emm_request(struct s_reader *);
-extern void logCWtoFile(ECM_REQUEST *er);
+extern void logCWtoFile(ECM_REQUEST *er, uchar *cw);
 extern void cs_log_config(void);
 extern void cs_close_log(void);
 extern int32_t cs_init_statistics();
@@ -412,7 +412,7 @@ extern void sort_stat(struct s_reader *rdr, int32_t reverse);
 extern int32_t reader_device_init(struct s_reader * reader);
 extern int32_t reader_checkhealth(struct s_reader * reader);
 extern void reader_post_process(struct s_reader * reader);
-extern int32_t reader_ecm(struct s_reader * reader, ECM_REQUEST *);
+extern int32_t reader_ecm(struct s_reader * reader, ECM_REQUEST *, struct s_ecm_answer *);
 extern int32_t reader_emm(struct s_reader * reader, EMM_PACKET *);
 extern int32_t reader_get_emm_type(EMM_PACKET *ep, struct s_reader * reader);
 extern struct s_cardsystem *get_cardsystem_by_caid(uint16_t caid);

globals.h
@@ -192,9 +192,6 @@
 #define R_IS_NETWORK    0x60
 #define R_IS_CASCADING  0xE0
 
-//ECM rc codes, reader only:
-#define E_RDR_NOTFOUND          0
-#define E_RDR_FOUND             1
 //ECM rc codes:
 #define E_FOUND			0
 #define E_CACHE1		1
@@ -514,6 +511,7 @@ typedef struct s_entitlement {			// contains entitlement Info
 struct s_client ;
 struct ecm_request_t ;
 struct emm_packet_t ;
+struct s_ecm_answer ;
 
 struct s_module {
 	int8_t			active;
@@ -587,12 +585,12 @@ struct s_cardreader
 struct s_cardsystem {
 	int8_t			active;
 	char			*desc;
-	int32_t			(*card_init)();
-	int32_t			(*card_info)();
-	int32_t			(*do_ecm)();
-	int32_t			(*do_emm)();
+	int32_t		(*card_init)();
+	int32_t		(*card_info)();
+	int32_t		(*do_ecm)(struct s_reader *, const struct ecm_request_t *, struct s_ecm_answer *);
+	int32_t		(*do_emm)(struct s_reader *, struct emm_packet_t *);
 	void			(*post_process)();
-	int32_t			(*get_emm_type)();
+	int32_t		(*get_emm_type)();
 	void			(*get_emm_filter)();
 	uchar			caids[2];
 };
@@ -625,8 +623,8 @@ typedef struct ecm_request_t {
 	int32_t			cpti;				// client pending table index
 	int32_t			stage;				// processing stage in server module
 	int32_t			level;				// send-level in client module
-	int32_t			rc;
-	uchar			rcEx;
+	int8_t			rc;
+	uint8_t			rcEx;
 	struct timeb	tps;				// incoming time stamp
 	uchar			locals_done;
 	int32_t			btun; 				// mark er as betatunneled
@@ -639,14 +637,27 @@ typedef struct ecm_request_t {
 #endif
 
 #ifdef MODULE_CCCAM
-	struct s_reader *origin_reader;
+	struct s_reader 	*origin_reader;
 	void			*origin_card; 		// CCcam preferred card!
 #endif
 
 	void			*src_data;
-	struct s_ecm	*ecmcacheptr;		// Pointer to ecm-cw-rc-cache!
+	struct s_ecm		*ecmcacheptr;		// Pointer to ecm-cw-rc-cache!
 	char			msglog[MSGLOGSIZE];
+	LLIST			*answer_list;
+	uint16_t		checksum;
+	struct ecm_request_t	*parent;
 } ECM_REQUEST;
+
+
+struct s_ecm_answer {
+	int8_t			status;
+	struct s_reader	*reader;
+	int8_t			rc;
+	uint8_t		rcEx;
+	uchar			cw[16];
+	char			msglog[MSGLOGSIZE];
+};
 
 #ifdef CS_ANTICASC
 struct s_acasc_shm {

module-cccam.c
@@ -693,11 +693,9 @@ int32_t cc_get_nxt_ecm(struct s_client *cl) {
 		else if (er->rc >= 10 && er->rc <= 100) { // stil active and waiting
 			pending++;
 			if (loop_check(cc->peer_node_id, er->client)) {
-				er->rc = E_RDR_NOTFOUND;
-				er->rcEx = E2_CCCAM_LOOP;
 				cs_debug_mask(D_READER, "%s ecm loop detected! client %s (%8X)", 
 						getprefix(), er->client->account->usr, er->client->thread);
-				write_ecm_answer(cl->reader, &cl->ecmtask[i]);
+				write_ecm_answer(cl->reader, &cl->ecmtask[i], E_NOTFOUND, E2_CCCAM_LOOP, NULL, NULL);
 			}
 			else 		
 			// search for the ecm with the lowest time, this should be the next to go
@@ -1023,11 +1021,9 @@ int32_t cc_send_ecm(struct s_client *cl, ECM_REQUEST *er, uchar *buf) {
 
 	if (!cc || (cl->pfd < 1) || !rdr->tcp_connected) {
 		if (er) {
-			er->rc = E_RDR_NOTFOUND;
-			er->rcEx = E2_CCCAM_NOCARD;
 			cs_debug_mask(D_READER, "%s server not init! ccinit=%d pfd=%d",
 					rdr->label, cc ? 1 : 0, cl->pfd);
-			write_ecm_answer(rdr, er);
+			write_ecm_answer(rdr, er, E_NOTFOUND, E2_CCCAM_NOCARD, NULL, NULL);
 		}
 		//cc_cli_close(cl);
 		return 0;
@@ -1105,8 +1101,7 @@ int32_t cc_send_ecm(struct s_client *cl, ECM_REQUEST *er, uchar *buf) {
 				rdr->available = 1;
 				cs_writeunlock(&cc->ecm_busy);
 			}
-			cur_er->rc = E_STOPPED;
-			write_ecm_answer(rdr, cur_er);
+			write_ecm_answer(rdr, cur_er, E_STOPPED, 0, NULL, NULL);
 			return 0;
 		}
         else {
@@ -1243,9 +1238,7 @@ int32_t cc_send_ecm(struct s_client *cl, ECM_REQUEST *er, uchar *buf) {
 		if (cc->last_msg != MSG_NEW_CARD && cc->last_msg != MSG_NEW_CARD_SIDINFO && cc->last_msg != MSG_CARD_REMOVED && !cc->just_logged_in) {
 			cs_debug_mask(D_READER, "%s no suitable card on server", getprefix());
 
-			cur_er->rc = E_RDR_NOTFOUND;
-			cur_er->rcEx = E2_CCCAM_NOCARD;
-			write_ecm_answer(rdr, cur_er);
+			write_ecm_answer(rdr, cur_er, E_NOTFOUND, E2_CCCAM_NOCARD, NULL, NULL);
 			//cur_er->rc = 1;
 			//cur_er->rcEx = 0;
 			//cs_sleepms(300);
@@ -2110,10 +2103,8 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l) {
 								if (cl->ecmtask[i].idx == ecm_idx) {
 										cs_debug_mask(D_TRACE, "%s forward card: %s", getprefix(), (buf[1]==MSG_CW_NOK1)?"NOK1":"NOK2");
 										ECM_REQUEST *er = &cl->ecmtask[i];
-										er->rc = E_RDR_NOTFOUND;
-										er->rcEx = (buf[1] == MSG_CW_NOK1)?E2_CCCAM_NOK1:E2_CCCAM_NOK2;
 										cl->pending--;
-										write_ecm_answer(rdr, er);
+										write_ecm_answer(rdr, er, E_NOTFOUND, (buf[1] == MSG_CW_NOK1)?E2_CCCAM_NOK1:E2_CCCAM_NOK2, NULL, NULL);
 										break;
 								}
 						}		

module-constcw.c
@@ -102,6 +102,7 @@ static int32_t constcw_send_ecm(struct s_client *client, ECM_REQUEST *er, uchar 
 {
     time_t t;
     struct s_reader *rdr = client->reader;
+    uchar cw[16];
 
     // FIXME
     msgbuf = msgbuf;
@@ -110,15 +111,11 @@ static int32_t constcw_send_ecm(struct s_client *client, ECM_REQUEST *er, uchar 
     // Check if DCW exist in the files
     //cs_log("Searching ConstCW for ECM: %04X:%06X:%04X (%d)", er->caid, er->prid, er->srvid, er->l);
    
-    if (constcw_analyse_file(er->caid, er->prid, er->srvid, er->cw)==0)
-    {
-        er->rc = E_RDR_NOTFOUND;
-        er->rcEx = E1_READER<<4 | E2_SID;
-	//cs_sleepms(100);
+    if (constcw_analyse_file(er->caid, er->prid, er->srvid, cw)==0) {
+        write_ecm_answer(rdr, er, E_NOTFOUND, (E1_READER<<4 | E2_SID), NULL, NULL);
+    } else {
+        write_ecm_answer(rdr, er, E_FOUND, 0, cw, NULL);
     }
-
-    //cs_sleepms(50);
-    write_ecm_answer(rdr, er);
     
     client->last = t;
     rdr->last_g = t;

module-gbox.c
@@ -938,27 +938,21 @@ static int32_t gbox_send_ecm(struct s_client *cli, ECM_REQUEST *er, uchar *buf)
   struct gbox_data *gbox = cli->gbox;
 
 	if (!gbox || !cli->reader->tcp_connected) {
-		er->rc = E_RDR_NOTFOUND;
-		er->rcEx = 0x27;
 		cs_debug_mask(D_READER, "gbox: %s server not init!", cli->reader->label);
-		write_ecm_answer(cli->reader, er);
+		write_ecm_answer(cli->reader, er, E_NOTFOUND, 0x27, NULL, NULL);
 
 		return 0;
 	}
 
 	if (!ll_count(gbox->peer.cards)) {
-		er->rc = E_RDR_NOTFOUND;
-		er->rcEx = 0x27;
 		cs_debug_mask(D_READER, "gbox: %s NO CARDS!", cli->reader->label);
-		write_ecm_answer(cli->reader, er);
+		write_ecm_answer(cli->reader, er, E_NOTFOUND, 0x27, NULL, NULL);
 		return 0;
 	}
 
 	if (!gbox->peer.online) {
-		er->rc = E_RDR_NOTFOUND;
-		er->rcEx = 0x27;
 		cs_debug_mask(D_READER, "gbox: peer is OFFLINE!");
-		write_ecm_answer(cli->reader, er);
+		write_ecm_answer(cli->reader, er, E_NOTFOUND, 0x27, NULL, NULL);
 		return 0;
 	}
 
@@ -1006,10 +1000,8 @@ static int32_t gbox_send_ecm(struct s_client *cli, ECM_REQUEST *er, uchar *buf)
   }
 
   if (!send_buf[16]) {
-		er->rc = E_RDR_NOTFOUND;
-		er->rcEx = 0x27;
 		cs_debug_mask(D_READER, "gbox: %s no suitable card found, discarding ecm", cli->reader->label);
-		write_ecm_answer(cli->reader, er);
+		write_ecm_answer(cli->reader, er, E_NOTFOUND, 0x27, NULL, NULL);
 
 		return 0;
   }

oscam-config.h
@@ -66,7 +66,7 @@
 #endif
 
 #ifndef MODULE_CAMD33
-#define MODULE_CAMD33
+//#define MODULE_CAMD33
 #endif
 
 #ifndef MODULE_CAMD35

oscam-log.c
@@ -327,7 +327,7 @@ void log_emm_request(struct s_reader *rdr){
  * This will consume a bit cpu time, but nothing has to be stored between
  * each call. If not file exists, a header is prepended
  */
-void logCWtoFile(ECM_REQUEST *er){
+void logCWtoFile(ECM_REQUEST *er, uchar *cw){
 	FILE *pfCWL;
 	char srvname[128];
 	/* %s / %s   _I  %04X  _  %s  .cwl  */
@@ -379,7 +379,7 @@ void logCWtoFile(ECM_REQUEST *er){
 	parity = er->ecm[0]&1;
 	fprintf(pfCWL, "%d ", parity);
 	for (i = parity * 8; i < 8 + parity * 8; i++)
-		fprintf(pfCWL, "%02X ", er->cw[i]);
+		fprintf(pfCWL, "%02X ", cw[i]);
 	/* better use incoming time er->tps rather than current time? */
 	strftime(buf,sizeof(buf),"%H:%M:%S\n", &timeinfo);
 	fprintf(pfCWL, "# %s", buf);

commit 0d3ee4721a89a54488db41e40a7066683d8ac422

Fix for CW1/2 swap, error introduced in r5739
git-svn-id: http://streamboard.gmc.to/svn/oscam/trunk@5740 4b0bc96b-bc66-0410-9d44-ebda105a78c1

reader-videoguard2.c
@@ -680,7 +680,7 @@ static int32_t videoguard2_do_ecm(struct s_reader * reader, const ECM_REQUEST *e
         do_post_dw_hash( ea->cw+8, &er->ecm[posB0-2]);
       }
 
-      if (reader->caid == 0x0907) { //quickfix: cw2 is not a vaild cw, something went wrong before
+      if (reader->caid == 0x0907) { //quickfix: cw2 is not a valid cw, something went wrong before
         memset(ea->cw+8, 0, 8);
         if (er->ecm[0] & 1) {
           memcpy(ea->cw+8, ea->cw, 8);
@@ -689,7 +689,7 @@ static int32_t videoguard2_do_ecm(struct s_reader * reader, const ECM_REQUEST *e
       } else {
         if(er->ecm[0]&1) {
           unsigned char tmpcw[8];
-          memcpy(tmpcw,er->cw+8,8);
+          memcpy(tmpcw,ea->cw+8,8);
           memcpy(ea->cw+8,ea->cw+0,8);
           memcpy(ea->cw+0,tmpcw,8);
         }